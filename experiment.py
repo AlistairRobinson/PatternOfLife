fbf2c0f8c9f49eefec15a3c5db95942c90b6115f
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-07-15 12:09:49 +0100

    Add trust calculation

--- experiment.py
+++ experiment.py
@@ -1,8 +1,25 @@
 from fcmlib import FCM
 import json
+import sys
 
 print("Initialising FCM")
-map=FCM(I_EMPLOYMENT=1, I_EDUCATION=-1, I_COMMERCIAL=-1, I_VOLUNTEERING=-1, I_RESIDENTIAL=-1, I_SPORTS=-1, I_CULTURAL=-1, I_TRAVEL=-1, 
+
+if len(sys.argv) < 9:
+    print("Insufficient arguments")
+    exit(0)
+
+w = [0, 0, 0, 0, 0, 0, 0, 0]
+r = [9, 9, 1, 3, 3, 3, 1, 1]
+
+for i in range(0, 8):
+    w[i] = int(sys.argv[i + 1]) * r[i]
+
+s = sum(w)
+
+for i in range(0, 8):
+    w[i] /= s
+
+map=FCM(I_EMPLOYMENT=w[0], I_EDUCATION=w[1], I_COMMERCIAL=w[2], I_VOLUNTEERING=w[3], I_RESIDENTIAL=w[4], I_SPORTS=w[5], I_CULTURAL=w[6], I_TRAVEL=w[7], 
         O_EMPLOYMENT=0, O_EDUCATION=0, O_COMMERCIAL=0, O_VOLUNTEERING=0, O_RESIDENTIAL=0, O_SPORTS=0, O_CULTURAL=0, O_TRAVEL=0)
 
 i_concepts = ["I_EMPLOYMENT", "I_EDUCATION", "I_COMMERCIAL", "I_VOLUNTEERING", "I_RESIDENTIAL", "I_SPORTS", "I_CULTURAL", "I_TRAVEL"]
@@ -34,3 +51,11 @@ for i in range(1, 60):
     map=FCM("./maps/experiment.json")
 
 print(map)
+
+trusted = FCM("./maps/trusted.json")
+s = 0
+
+for o in o_concepts:
+    s += abs(trusted[o].value - map[o].value)
+
+print("Trust = {}".format((1 - s/8)**2))
