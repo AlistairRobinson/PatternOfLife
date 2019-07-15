3110efa3b3db5e46647d15dee11735e44caa683d
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-07-15 15:41:09 +0100

    Add save function

--- experiment.py
+++ experiment.py
@@ -23,7 +23,7 @@ opt = {
 }
 
 def authenticate(inputs={"I_EMPLOYMENT": 0, "I_EDUCATION": 0, "I_COMMERCIAL": 0, "I_VOLUNTEERING": 0, "I_RESIDENTIAL": 0, "I_SPORTS": 0, "I_CULTURAL": 0, "I_TRAVEL": 0}, 
-                 weights="./weights.json", file="./maps/experiment.json", trusted="./maps/trusted.json"):
+                 trusted="./maps/trusted.json", file="./maps/experiment.json", weights="./weights.json", ):
 
     i = 0
     for k in inputs.keys():
@@ -74,6 +74,45 @@ def authenticate(inputs={"I_EMPLOYMENT": 0, "I_EDUCATION": 0, "I_COMMERCIAL": 0,
 
     return (1 - s/16)**2
 
+def save(inputs={"I_EMPLOYMENT": 0, "I_EDUCATION": 0, "I_COMMERCIAL": 0, "I_VOLUNTEERING": 0, "I_RESIDENTIAL": 0, "I_SPORTS": 0, "I_CULTURAL": 0, "I_TRAVEL": 0}, 
+         file="./maps/experiment.json", weights="./weights.json"):
+    i = 0
+    for k in inputs.keys():
+        w[i] = inputs[k]
+        i += 1
+
+    s = sum(w) + 1
+
+    for i in range(0, 8):
+        w[i] /= s
+    
+
+    map=FCM(I_EMPLOYMENT=w[0], I_EDUCATION=w[1], I_COMMERCIAL=w[2], I_VOLUNTEERING=w[3], I_RESIDENTIAL=w[4], I_SPORTS=w[5], I_CULTURAL=w[6], I_TRAVEL=w[7], 
+            O_EMPLOYMENT=0, O_EDUCATION=0, O_COMMERCIAL=0, O_VOLUNTEERING=0, O_RESIDENTIAL=0, O_SPORTS=0, O_CULTURAL=0, O_TRAVEL=0)
+
+    i_concepts = ["I_EMPLOYMENT", "I_EDUCATION", "I_COMMERCIAL", "I_VOLUNTEERING", "I_RESIDENTIAL", "I_SPORTS", "I_CULTURAL", "I_TRAVEL"]
+    o_concepts = ["O_EMPLOYMENT", "O_EDUCATION", "O_COMMERCIAL", "O_VOLUNTEERING", "O_RESIDENTIAL", "O_SPORTS", "O_CULTURAL", "O_TRAVEL"]
+
+    for i in i_concepts:
+        for o in o_concepts:
+            map.connect(i, o)
+            map[o].relation.set(i, 1)
+
+    data = json.loads(open(weights).read())['w']
+
+    for i in data:
+        for k1 in i.keys():
+            for j in i[k1]:
+                for k2 in j.keys():
+                    if j[k2]['wp'] == 0:
+                        map[k2].relation.set(k1, j[k2]['p'])
+                    else:
+                        map[k2].relation.set(k1, j[k2]['wp'])
+    
+    for i in range(1, 60):
+        map.update()
+        map.save(file)
+        map=FCM(file)
 
 def main():
 
