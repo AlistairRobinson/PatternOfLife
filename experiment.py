e3aef75e125f7eae33781dca81a60649c65b5862
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-07-18 11:11:16 +0100

    Improve code quality dramatically

--- experiment.py
+++ experiment.py
@@ -8,123 +8,118 @@ from fcmlib import FCM
 import json
 import sys
 
-verbose = False
-
 w = [0, 0, 0, 0, 0, 0, 0, 0]
 r = [9, 9, 1, 3, 3, 3, 1, 1]
 
 i_concepts = ["I_EMPLOYMENT", "I_EDUCATION", "I_COMMERCIAL", "I_VOLUNTEERING", "I_RESIDENTIAL", "I_SPORTS", "I_CULTURAL", "I_TRAVEL"]
 o_concepts = ["O_EMPLOYMENT", "O_EDUCATION", "O_COMMERCIAL", "O_VOLUNTEERING", "O_RESIDENTIAL", "O_SPORTS", "O_CULTURAL", "O_TRAVEL"]
 
-def set_verbose():
-    global verbose
-    verbose = True
-    print("Toggled verbose")
+iterations = 60
 
-opt = {
-    "-v": set_verbose
-}
+def construct(l:list, weights:str="./weights.json") -> FCM:
+    if len(l) < 8:
+        return connect(FCM(
+            I_EMPLOYMENT   = 0, 
+            I_EDUCATION    = 0, 
+            I_COMMERCIAL   = 0, 
+            I_VOLUNTEERING = 0, 
+            I_RESIDENTIAL  = 0, 
+            I_SPORTS       = 0, 
+            I_CULTURAL     = 0, 
+            I_TRAVEL       = 0,
+            O_EMPLOYMENT   = 0, 
+            O_EDUCATION    = 0,
+            O_COMMERCIAL   = 0,
+            O_VOLUNTEERING = 0, 
+            O_RESIDENTIAL  = 0,
+            O_SPORTS       = 0,
+            O_CULTURAL     = 0,
+            O_TRAVEL       = 0
+        ), weights)
+    else:
+        return connect(FCM(
+            I_EMPLOYMENT   = l[0], 
+            I_EDUCATION    = l[1], 
+            I_COMMERCIAL   = l[2], 
+            I_VOLUNTEERING = l[3], 
+            I_RESIDENTIAL  = l[4], 
+            I_SPORTS       = l[5], 
+            I_CULTURAL     = l[6], 
+            I_TRAVEL       = l[7],
+            O_EMPLOYMENT   = 0, 
+            O_EDUCATION    = 0,
+            O_COMMERCIAL   = 0,
+            O_VOLUNTEERING = 0, 
+            O_RESIDENTIAL  = 0,
+            O_SPORTS       = 0,
+            O_CULTURAL     = 0,
+            O_TRAVEL       = 0
+        ), weights)
 
-def construct(a, b, c, d, e, f, g, h):
-    return {
-        "I_EMPLOYMENT": a, 
-        "I_EDUCATION": b, 
-        "I_COMMERCIAL": c, 
-        "I_VOLUNTEERING": d, 
-        "I_RESIDENTIAL": e, 
-        "I_SPORTS": f, 
-        "I_CULTURAL": g, 
-        "I_TRAVEL": h
-    }
+def construct(d:dict, weights:str="./weights.json") -> FCM:
+    if set(d.keys()) == set(i_concepts):
+        return connect(FCM(
+            I_EMPLOYMENT   = 0, 
+            I_EDUCATION    = 0, 
+            I_COMMERCIAL   = 0, 
+            I_VOLUNTEERING = 0, 
+            I_RESIDENTIAL  = 0, 
+            I_SPORTS       = 0, 
+            I_CULTURAL     = 0, 
+            I_TRAVEL       = 0,
+            O_EMPLOYMENT   = 0, 
+            O_EDUCATION    = 0,
+            O_COMMERCIAL   = 0,
+            O_VOLUNTEERING = 0, 
+            O_RESIDENTIAL  = 0,
+            O_SPORTS       = 0,
+            O_CULTURAL     = 0,
+            O_TRAVEL       = 0
+        ), weights)
+    else:
+        return connect(FCM(
+            I_EMPLOYMENT   = d['I_EMPLOYMENT'], 
+            I_EDUCATION    = d['I_EDUCATION'], 
+            I_COMMERCIAL   = d['I_COMMERCIAL'], 
+            I_VOLUNTEERING = d['I_VOLUNTEERING'], 
+            I_RESIDENTIAL  = d['I_RESIDENTIAL'], 
+            I_SPORTS       = d['I_SPORTS'], 
+            I_CULTURAL     = d['I_CULTURAL'], 
+            I_TRAVEL       = d['I_TRAVEL'],
+            O_EMPLOYMENT   = 0, 
+            O_EDUCATION    = 0,
+            O_COMMERCIAL   = 0,
+            O_VOLUNTEERING = 0, 
+            O_RESIDENTIAL  = 0,
+            O_SPORTS       = 0,
+            O_CULTURAL     = 0,
+            O_TRAVEL       = 0
+        ), weights)
 
-def authenticate(inputs={"I_EMPLOYMENT": 0, "I_EDUCATION": 0, "I_COMMERCIAL": 0, "I_VOLUNTEERING": 0, "I_RESIDENTIAL": 0, "I_SPORTS": 0, "I_CULTURAL": 0, "I_TRAVEL": 0}, 
-                 trusted="./maps/trusted.json", file="./maps/experiment.json", weights="./weights.json"):
+def normalise(l:list) -> list:
+    s = sum(l) + 1
+    for i in range(0, len(l)):
+        l[i] /= s
+    return l
 
-    i = 0
-    for k in inputs.keys():
-        w[i] = inputs[k]
-        i += 1
+def trust(a:FCM, b:FCM) -> float:
 
-    s = sum(w) + 1
-
-    for i in range(0, 8):
-        w[i] /= s
-    
-
-    map = FCM(I_EMPLOYMENT=w[0], I_EDUCATION=w[1], I_COMMERCIAL=w[2], I_VOLUNTEERING=w[3], I_RESIDENTIAL=w[4], I_SPORTS=w[5], I_CULTURAL=w[6], I_TRAVEL=w[7], 
-              O_EMPLOYMENT=0, O_EDUCATION=0, O_COMMERCIAL=0, O_VOLUNTEERING=0, O_RESIDENTIAL=0, O_SPORTS=0, O_CULTURAL=0, O_TRAVEL=0)
-
-    for i in i_concepts:
-        for o in o_concepts:
-            map.connect(i, o)
-            map[o].relation.set(i, 1)
-
-    data = json.loads(open(weights).read())['w']
-
-    for i in data:
-        for k1 in i.keys():
-            for j in i[k1]:
-                for k2 in j.keys():
-                    if j[k2]['wp'] == 0:
-                        map[k2].relation.set(k1, j[k2]['p'])
-                    else:
-                        map[k2].relation.set(k1, j[k2]['wp'])
-    
-    for i in range(1, 60):
-        map.update()
-        map.save(file)
-        map = FCM(file)
-
-    trusted = FCM(trusted)
     s = 0
 
     for i in i_concepts:
-        s += abs(trusted[i].value - map[i].value)
+        s += abs(a[i].value - b[i].value)
 
     for o in o_concepts:
-        s += abs(trusted[o].value - map[o].value)
-
-    return (1 - s/16)**2
-
-def authenticate_from_file(file="./maps/experiment.json", trusted="./maps/trusted.json", weights="./weights.json"):
-
-    map = FCM(file)
-    trusted = FCM(trusted)
-
-    s = 0
-
-    for i in i_concepts:
-        s += abs(trusted[i].value - map[i].value)*10
-
-    for o in o_concepts:
-        s += abs(trusted[o].value - map[o].value)*10
+        s += abs(a[o].value - b[o].value)
 
     return (1 - s/16)
 
-def conformity(inputs={"I_EMPLOYMENT": 0, "I_EDUCATION": 0, "I_COMMERCIAL": 0, "I_VOLUNTEERING": 0, "I_RESIDENTIAL": 0, "I_SPORTS": 0, "I_CULTURAL": 0, "I_TRAVEL": 0}, 
-               conformity="./maps/conformity.json"):
-    return authenticate(inputs, conformity)
-
-def save(inputs={"I_EMPLOYMENT": 0, "I_EDUCATION": 0, "I_COMMERCIAL": 0, "I_VOLUNTEERING": 0, "I_RESIDENTIAL": 0, "I_SPORTS": 0, "I_CULTURAL": 0, "I_TRAVEL": 0}, 
-         file="./maps/experiment.json", weights="./weights.json"):
-    i = 0
-    for k in inputs.keys():
-        w[i] = inputs[k]
-        i += 1
-
-    s = sum(w) + 1
-
-    for i in range(0, 8):
-        w[i] /= s
-    
-
-    map = FCM(I_EMPLOYMENT=w[0], I_EDUCATION=w[1], I_COMMERCIAL=w[2], I_VOLUNTEERING=w[3], I_RESIDENTIAL=w[4], I_SPORTS=w[5], I_CULTURAL=w[6], I_TRAVEL=w[7], 
-              O_EMPLOYMENT=0, O_EDUCATION=0, O_COMMERCIAL=0, O_VOLUNTEERING=0, O_RESIDENTIAL=0, O_SPORTS=0, O_CULTURAL=0, O_TRAVEL=0)
+def connect(new:FCM, weights:str = "./weights.json") -> FCM:
 
     for i in i_concepts:
         for o in o_concepts:
-            map.connect(i, o)
-            map[o].relation.set(i, 1)
+            new.connect(i, o)
+            new[o].relation.set(i, 1)
 
     data = json.loads(open(weights).read())['w']
 
@@ -133,21 +128,54 @@ def save(inputs={"I_EMPLOYMENT": 0, "I_EDUCATION": 0, "I_COMMERCIAL": 0, "I_VOLU
             for j in i[k1]:
                 for k2 in j.keys():
                     if j[k2]['wp'] == 0:
-                        map[k2].relation.set(k1, j[k2]['p'])
+                        new[k2].relation.set(k1, j[k2]['p'])
                     else:
-                        map[k2].relation.set(k1, j[k2]['wp'])
+                        new[k2].relation.set(k1, j[k2]['wp'])
     
-    for i in range(1, 60):
-        map.update()
-        map.save(file)
-        map = FCM(file)
+    return new
 
-    print(map)
-    print("FCM saved to " + file + " successfully")
+def authenticate(new:FCM, trusted:FCM, file:str = "./maps/experiment.json", weights:str = "./weights.json", verbose:bool = False) -> float:
+    
+    for i in range(1, iterations):
+        new.update()
+        new.save(file)
+        new = FCM(file)
+
+    if verbose:
+        print("Newly generated FCM: ")
+        print(new)
+        print("Trusted FCM: ")
+        print(trusted)
+
+    return trust(new, trusted)
+
+def conformity(new:FCM, conformity:str="./maps/conformity.json"):
+
+    return authenticate(inputs, FCM(conformity))
+
+def save(new:FCM, file:str = "./maps/experiment.json", weights:str = "./weights.json", verbose:bool = False) -> bool:
+    
+    for i in range(1, iterations):
+        new.update()
+        new.save(file)
+        new = FCM(file)
+
+    if verbose:
+        print(new)
+        print("FCM saved to " + file + " successfully")
+
+    return True 
 
 def main():
 
-    global verbose
+    verbose = False
+
+    def set_verbose():
+        verbose = True
+
+    opt = {
+        "-v": set_verbose
+    }
 
     if len(sys.argv) < 9:
         print("Insufficient arguments")
@@ -156,10 +184,7 @@ def main():
     for i in range(0, 8):
         w[i] = int(sys.argv[i + 1]) * r[i]
 
-    s = sum(w) + 1
-
-    for i in range(0, 8):
-        w[i] /= s
+    w = normalise(w)
 
     l = 9
 
@@ -167,50 +192,24 @@ def main():
         opt[sys.argv[l]]()
         l += 1
 
-    if verbose:
-        print("Initialising FCM")
-
-    map = FCM(I_EMPLOYMENT=w[0], I_EDUCATION=w[1], I_COMMERCIAL=w[2], I_VOLUNTEERING=w[3], I_RESIDENTIAL=w[4], I_SPORTS=w[5], I_CULTURAL=w[6], I_TRAVEL=w[7], 
-              O_EMPLOYMENT=0, O_EDUCATION=0, O_COMMERCIAL=0, O_VOLUNTEERING=0, O_RESIDENTIAL=0, O_SPORTS=0, O_CULTURAL=0, O_TRAVEL=0)
-
-    for i in i_concepts:
-        for o in o_concepts:
-            map.connect(i, o)
-            map[o].relation.set(i, 1)
-
-    data = json.loads(open('weights.json').read())['w']
-
-    for i in data:
-        for k1 in i.keys():
-            for j in i[k1]:
-                for k2 in j.keys():
-                    if j[k2]['wp'] == 0:
-                        map[k2].relation.set(k1, j[k2]['p'])
-                    else:
-                        map[k2].relation.set(k1, j[k2]['wp'])
+    new = construct(w)
 
     if verbose:
-        print(map)
-        print("Processing FCM:")
+        print("Initialised FCM:")
+        print(new)
+        print("Processing FCM for {} iterations:".format(iterations))
 
-    for i in range(1, 60):
-        map.update()
-        map.save("./maps/experiment.json")
-        map = FCM("./maps/experiment.json")
+    for i in range(1, iterations):
+        new.update()
+        new.save("./maps/experiment.json")
+        new = FCM("./maps/experiment.json")
 
     if verbose:
-        print(map)
+        print(new)
 
     trusted = FCM("./maps/trusted.json")
-    s = 0
 
-    for i in i_concepts:
-        s += abs(trusted[i].value - map[i].value)
-
-    for o in o_concepts:
-        s += abs(trusted[o].value - map[o].value)
-
-    print("Trust = {}".format((1 - s/16)**2))
+    print("Trust = {}".format(trust(new, trusted)))
 
 if __name__ == "__main__":
     main()
