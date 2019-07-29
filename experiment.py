5c492bc0ded2a2ed33ad5353d810ed4623c2cfd8
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-07-29 14:52:26 +0100

    Encapsulate functionality

--- experiment.py
+++ experiment.py
@@ -12,136 +12,132 @@ import sys
 from pcapfile import savefile
 from fcmlib import FCM
 
-devices = {}
-ssids = {}
-threshold = 0
-limit = 418
-verbose = False
+def main():
 
-for arg in sys.argv:
-    if arg == "-v":
-        verbose = True
-    if arg == "-l":
-        limit = int(sys.argv[int(sys.argv.index("-l") + 1)])
+    devices = {}
+    ssids = {}
+    threshold = 0
+    limit = 418
+    verbose = False
+    save_file = ""
 
-if verbose:
-    print("\nLoading {} files...".format(limit))
+    for arg in sys.argv:
+        if arg == "-v":
+            verbose = True
+        if arg == "-l":
+            limit = int(sys.argv[int(sys.argv.index("-l") + 1)])
+        if arg == "-s":
+            save_file = sys.argv[int(sys.argv.index("-s") + 1)]
 
-for i in range(0, limit):
 
     if verbose:
-        print("\r\tLoading file {}...".format(i), end="")
+        print("\nLoading {} files...".format(limit))
 
-    pcap = savefile.load_savefile(open('./data/probes-2013-03-28.pcap{}'.format(i), 'rb'))
+    for i in range(0, limit):
 
-    for packet in pcap.packets:
+        if verbose:
+            print("\r\tLoading file {}...".format(i), end="")
 
-        # Source address [56:68]
-        src = packet.raw().hex()[56:68]
+        pcap = savefile.load_savefile(open('./data/probes-2013-03-28.pcap{}'.format(i), 'rb'))
 
-        # Tag length [86:88]
-        tag_length = packet.raw().hex()[86:88]
+        for packet in pcap.packets:
 
-        if tag_length == "00":
-            continue
+            # Source address [56:68]
+            src = packet.raw().hex()[56:68]
 
-        # SSID [88:110]
-        ssid = bytes.fromhex(packet.raw().hex()[88:110]).decode('utf-8')
+            # Tag length [86:88]
+            tag_length = packet.raw().hex()[86:88]
 
-        if devices.get(src) is None:
-            devices[src] = {ssid}
-        else:
-            devices.get(src).add(ssid)
+            if tag_length == "00":
+                continue
 
-if verbose:
-    print("\r\tSuccessfully loaded {} files\n".format(limit))
-    print("Populating SSID datastructures...")
+            # SSID [88:110]
+            ssid = bytes.fromhex(packet.raw().hex()[88:110]).decode('utf-8')
 
-for device in list(devices.keys()):
+            if devices.get(src) is None:
+                devices[src] = {ssid}
+            else:
+                devices.get(src).add(ssid)
 
-    if len(devices.get(device)) < threshold:
-        del devices[device]
-
-    for ssid in devices.get(device, []):
-        if ssids.get(ssid) is None:
-            ssids[ssid] = {device}
-        else:
-            ssids.get(ssid).add(device)
-
-if verbose:
-    print("Sorting SSID datastructures...")
-
-sorted_ssids = sorted(ssids, key=lambda k: len(ssids[k]), reverse=True)
-
-if verbose:
-    print("\nProcessing SSID weights...")
-
-weights = {}
-
-for ssid_a in sorted_ssids:
     if verbose:
-        print("\r\tProcessing {}...".format(ssid_a), end="")
-    weight_sum = 1
-    weights[ssid_a] = {}
-    for device in ssids[ssid_a]:
-        for ssid_b in devices[device]:
-            if ssid_a != ssid_b:
-                if ssid_b in weights[ssid_a].keys():
-                    weights[ssid_a][ssid_b] += 1
-                else:
-                    weights[ssid_a][ssid_b] = 1
-                weight_sum += 1
-    for ssid_b in weights[ssid_a].keys():
-        weights[ssid_a][ssid_b] /= weight_sum
-    weights[ssid_a][ssid_a] = 1
+        print("\r\tSuccessfully loaded {} files\n".format(limit))
+        print("Populating SSID datastructures...")
 
-if verbose:
-    print("\r\tSuccessfully processed SSID weights\n")
-    print("Populating FCM...")
+    for device in list(devices.keys()):
 
-map = FCM()
+        if len(devices.get(device)) < threshold:
+            del devices[device]
 
-for ssid in sorted_ssids:
+        for ssid in devices.get(device, []):
+            if ssids.get(ssid) is None:
+                ssids[ssid] = {device}
+            else:
+                ssids.get(ssid).add(device)
+
     if verbose:
-        print("\r\tAdding {}...".format(ssid), end="")
-    map.add("I_" + ssid)
-    map.add("O_" + ssid)
+        print("Sorting SSID datastructures...")
 
-if verbose:
-    print("\r\tSuccessfully populated FCM\n")
-    print("Connecting FCM...")
+    sorted_ssids = sorted(ssids, key=lambda k: len(ssids[k]), reverse=True)
 
-for ssid_a in sorted_ssids:
-    print("\r\tConnecting {}".format(ssid_a), end="")
-    for ssid_b in weights[ssid_a].keys():
-        weight = weights[ssid_a][ssid_b]
-        if weight > 0:
-            map.connect("I_" + ssid_a, "O_" + ssid_b)
-            map["O_" + ssid_b].relation.set("I_" + ssid_a, weight)
+    if verbose:
+        print("\nProcessing SSID weights...")
 
-if verbose:
-    print("\r\tSuccessfully connected FCM\n")
-    print("Saving FCM...")
+    weights = {}
 
-map.save("maps/new.json")
+    for ssid_a in sorted_ssids:
+        if verbose:
+            print("\r\tProcessing {}...".format(ssid_a), end="")
+        weight_sum = 0
+        weights[ssid_a] = {}
+        for device in ssids[ssid_a]:
+            for ssid_b in devices[device]:
+                if ssid_a != ssid_b:
+                    if ssid_b in weights[ssid_a].keys():
+                        weights[ssid_a][ssid_b] += 1
+                    else:
+                        weights[ssid_a][ssid_b] = 1
+                    weight_sum += 1
+        for ssid_b in weights[ssid_a].keys():
+            weights[ssid_a][ssid_b] /= weight_sum
+        weights[ssid_a][ssid_a] = 1
 
-print("Successfully saved FCM\n")
+    if verbose:
+        print("\r\tSuccessfully processed SSID weights\n")
+        print("Populating FCM...")
 
-#print(map)
-#print(weights)
+    map = FCM()
 
-"""
+    for ssid in sorted_ssids:
+        if verbose:
+            print("\r\tAdding {}...".format(ssid), end="")
+        map.add("I_" + ssid)
+        map.add("O_" + ssid)
 
-#print(devices)
-#print(len(devices))
+    if verbose:
+        print("\r\tSuccessfully populated FCM\n")
+        print("Connecting FCM...")
 
-print("Index, Name, Size")
-index = 0
+    for ssid_a in sorted_ssids:
+        print("\r\tConnecting {}".format(ssid_a), end="")
+        for ssid_b in weights[ssid_a].keys():
+            weight = weights[ssid_a][ssid_b]
+            if weight > 0:
+                map.connect("I_" + ssid_a, "O_" + ssid_b)
+                map["O_" + ssid_b].relation.set("I_" + ssid_a, weight)
 
-for ssid in sorted_ssids:
-    print("{}, {}, {}".format(index, ssid, len(ssids.get(ssid))))
-    index += 1
+    if verbose:
+        print("\r\tSuccessfully connected FCM\n")
 
-"""
+    if save_file != "":
+        if verbose:
+            print("Saving FCM...")
+        map.save(save_file)
+        if verbose:
+            print("Successfully saved FCM\n")
 
-print("Program exiting...")
+    print("Program exiting...")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
